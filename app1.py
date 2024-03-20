8c6f1114f436936f9288341a263397013112add4
Akis  Gazepidis <akis.gazepidis@d-one.ai>
2024-03-20 22:30:06 +0200

    Add app0 and app1 files

---
+++ app1.py
@@ -0,0 +1,17 @@
+from kafka import KafkaConsumer
+from pymongo import MongoClient
+import json
+
+# Kafka consumer for partition 1
+consumer = KafkaConsumer('Blocks', bootstrap_servers='localhost:9999', group_id='group1')
+
+# MongoDB connection
+client = MongoClient('localhost', 27017)
+db = client['blockchain']
+blocks_collection = db['blocks']
+
+# Read from Kafka and write to MongoDB
+for message in consumer:
+    block_data = json.loads(message.value.decode('utf-8'))
+    if block_data['block_serial_number'] % 2 != 0:
+        blocks_collection.insert_one(block_data)
